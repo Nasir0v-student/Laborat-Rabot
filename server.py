e6251581561fb13e8026fe34c449b84f11a80d27
hollyjammer2 <164227037+hollyjammer2@users.noreply.github.com>
2025-04-02 09:26:58 +0500

    Update server.py
    Внедрили возможность добавления пользователей в базу данных.

--- server.py
+++ server.py
@@ -1,32 +1,55 @@
-from flask import Flask, Response, request, jsonify
+
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text, bindparam
 import json
+connection_string = "mysql+pymysql://kostya:daniil123!@192.168.50.127:3306/calculator+"
+engine = create_engine(connection_string, echo=True)
+
+
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
 @app.route("/")
 def index():
-    # Возвращаем JSON-ответ с данными из API
-    return jsonify({
-        "message": "Request1",
-        "articles": [
-            {
-                "Name": "Vasya",
-                "description": "2 + 3 = 5",
-            },
-        ]
-    })
-    
-@app.route("/api/article/all")
-def get_article():
-    article = [
-        {
-            "History": "Vasya",
-            "description": "2 + 3 = 5",
-        },
-    ]
-    return Response(json.dumps(article), content_type="application/json")
+    return "Hello world"
+
+
+
+
+@app.route("/api/user/all")
+def get_products():
+    with engine.connect() as connection:
+        raw_result = connection.execute(text("SELECT * FROM `calculator+`"))
+        result = []
+        for r in raw_result.all():
+            result.append(r._asdict())
+        return jsonify(result)
+    return Response(jsonify({"status": "500", "message": "Database is down!"}), status=500)
+
+
+
+
+
+@app.route("/api/user", methods=["POST"])
+def add_product():
+    if request.method == "POST":
+        form = request.form
+        with engine.connect() as connection:
+            query = text("INSERT INTO `calculator+` (Nickname, Email, Password) VALUES (:Nickname, :Email, :Password) RETURNING *")
+            query = query.bindparams(bindparam("Nickname", form.get("Nickname")))
+            query = query.bindparams(bindparam("Email", form.get("Email")))
+            query = query.bindparams(bindparam("Password", form.get("Password")))
+            result = connection.execute(query)
+            connection.commit()
+            return jsonify(result.fetchone()._asdict())
+        return jsonify({"message": "Error"})
+
+
+
+
+
 def main():
     app.run("localhost", 8000, debug=True)
 
