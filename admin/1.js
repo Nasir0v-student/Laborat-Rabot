f4e55e566ccf4070acfa5c7f077ec2f41bcf8c8a
hollyjammer2 <164227037+hollyjammer2@users.noreply.github.com>
2025-04-15 10:18:18 +0500

    Update 1.js
    Добавили код для удаления пользователей

--- admin/1.js
+++ admin/1.js
@@ -15,19 +15,34 @@ async function render_calculator() {
         <td>{Nickname}</td>
         <td>{Email}</td>
         <td>{Password}
+        <td>
+        <button class="btn btn-danger" onclick="delete_user({id})">🗑️</button> 
+        </td>
     </tr>`;
 
     let calculator = await get_calculator();
     let container = document.getElementById("calculator+");
     calculator.forEach(element => {
         let calculator = template
-            .replace("{id}", element.id)
+            .replaceAll("{id}", element.id)
             .replace("{Nickname}", element.Nickname)
             .replace("{Email}", element.Email)
             .replace("{Password}", element.Password)
           
         container.innerHTML += calculator;
     });
+}
+
+async function delete_user(id) {
+    let response = await fetch(`http://localhost:8000/api/user/${id}`, {
+        method: 'DELETE'
+    });
+    
+    if (response.ok) {
+        window.location.reload(false);
+    } else {
+        alert("Ошибка http: " + response.status);
+    }
 }
 
 render_calculator();
